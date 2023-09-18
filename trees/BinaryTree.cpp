2c1aa4cab2967a5a9293e1adf28e0fb23503e861
Harsh-bhai <107921110+Harsh-bhai@users.noreply.github.com>
2023-09-18 22:41:23 +0530

    completed trees

--- trees/BinaryTree.cpp
+++ trees/BinaryTree.cpp
@@ -11,7 +11,7 @@ class TreeNode{
 class BinaryTree{
     private:
         TreeNode* root;
-        queue<int*> q ;
+        queue<TreeNode**> q ;
         
     
     public:
@@ -22,10 +22,10 @@ class BinaryTree{
             root->right=NULL;
             cout << "Enter root :" << endl;
             cin >> root->data;
-            q.push(&(root->data));
+            q.push(&(root));
             while(!q.empty()){
                 TreeNode *t;
-                int* p=q.front();
+                TreeNode** p=q.front();
                 q.pop();
                 int ldata,rdata;
                 cout << "Enter Left child:" << endl;
@@ -34,8 +34,8 @@ class BinaryTree{
                     t=new TreeNode;
                     t->data=ldata;
                     t->left=t->right=NULL;
-                    root->left=t;
-                    q.push(&ldata);
+                    (*p)->left=t;
+                    q.push(&(t));
                 }
                 cout << "Enter Right child:" << endl;
                 cin >> rdata;
@@ -43,8 +43,8 @@ class BinaryTree{
                     t=new TreeNode;
                     t->data=rdata;
                     t->left=t->right=NULL;
-                    root->right=t;
-                    q.push(&rdata);
+                    (*p)->right=t;
+                    q.push(&t);
                 }
             }   
         }
@@ -94,13 +94,150 @@ class BinaryTree{
         }
     }
     
+    void iterativePreOrder(){
+        if(root){
+            cout << "Iterative Pre Order :" << endl;
+            iterativePreOrder(root);
+        }
+    }
+    void iterativePreOrder(TreeNode * node){
+        stack<TreeNode*> s;
+        while(node!=NULL || s.empty()){
+            if(node){
+                cout << node->data << " ";
+                s.push(node);
+                node=node->left;
+            }
+            else{
+                node =s.top();
+                s.pop();
+                node=node->right;
+            }
+        }
+    }
+
+    void levelOrder(){
+        cout << "Level Order Traversal :" << endl;
+        levelOrder(root);
+    }
+
+    void levelOrder(TreeNode * node){
+        queue<TreeNode *> q;
+        cout << node->data << " ";
+        q.push(node);
+        while(!q.empty()){
+            node=q.front();
+            q.pop();
+            if(node->left){
+                cout << node->left->data << " ";
+                q.push(node->left);
+            }
+            if(node->right){
+                cout << node->right->data << " ";
+                q.push(node->right);
+            }
+        }
+    }
+
+int sum(){
+    return sum(root);
+}
+int sum(TreeNode * node){
+    int leftCount,rightCount;
+    if(node){
+        leftCount=sum(node->left);
+        rightCount=sum(node->right);
+        return leftCount+rightCount+node->data;
+    }
+    return 0;
+}
+int height(){
+    return height(root);
+}
+int height(TreeNode * node){
+    int leftHeight=0,rightHeight=0;
+    if(node){
+        leftHeight=height(node->left);
+        rightHeight=height(node->right);
+        if(leftHeight>rightHeight)
+            return leftHeight+1;
+        return rightHeight+1;
+    }
+    return 0;
+}
+int countNode(){
+    return countNode(root);
+}
+int countNode(TreeNode * node){
+    int leftCount,rightCount;
+    if(node){
+        leftCount=countNode(node->left);
+        rightCount=countNode(node->right);
+        return leftCount+rightCount+1;
+    }
+    return 0;
+}
+int leafNodes(){
+    return leafNodes(root);
+}
+int leafNodes(TreeNode * node){
+    int leftCount,rightCount;
+    if(node){
+        leftCount=leafNodes(node->left);
+        rightCount=leafNodes(node->right);
+        if(node->left && node->right)
+            return leftCount+rightCount;
+        return leftCount+rightCount+1;
+    }
+    return 0;
+}
+int degree2(){
+    return degree2(root);
+}
+int degree2(TreeNode * node){
+    int leftCount,rightCount;
+    if(node){
+        leftCount=degree2(node->left);
+        rightCount=degree2(node->right);
+        if(node->left && node->right)
+            return leftCount+rightCount+1;
+        return leftCount+rightCount;
+    }
+    return 0;
+}
+// int internalNodes(){
+//     return internalNodes(root);
+// }
+// int internalNodes(TreeNode * node){
+//     int leftCount,rightCount;
+//     if(node){
+//         leftCount=internalNodes(node->left);
+//         rightCount=internalNodes(node->right);
+//         if(node->left || node->right)
+//             return leftCount+rightCount;
+//         return leftCount+rightCount+1;
+//     }
+//     return 0;
+// }
+//  This is a template function, u can calculate height,no of 2 degree,1 degree nodes etc.
+// int countNode(){        
+//     return countNode(root);
+// }
+// int countNode(TreeNode * node){
+//     int leftCount,rightCount;
+//     if(node){
+//         leftCount=countNode(node->left);
+//         rightCount=countNode(node->right);
+//         return leftCount+rightCount+1;
+//     }
+//     return 0;
+// }
 
 };
 int main() {
     BinaryTree b;
     b.initBinaryTree();
-    b.preOrder();
-    b.inOrder();
     b.postOrder();
+    cout<<b.internalNodes();
     return 0;
 }
